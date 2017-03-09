ce7d92f66cce5316dd5ad867c428e5f3220835fb
williamliuc <1476996891@qq.com>
2017-03-09 21:50:54 +0800

    Update test_logic.py

--- logRegression/test_logic.py
+++ logRegression/test_logic.py
@@ -1,21 +1,36 @@
-from numpy import *
-def loaddata():
-    train_x=[]
-    train_y=[]
-    fileIn=open('C:/Users/lc/.spyder/logic/test.txt')
-    for line in fileIn.readlines():
-        alow=line.strip().split()#split()函数默认从空格处分开
-        train_x.append([alow[0],alow[1]])
-        train_y.append(alow[2])
-    return mat(train_x),mat(train_y).transpose()
+# -*- coding: utf-8 -*-
 
-print "load data..."
-
-train_x,train_y=loaddata()
-print train_x
-numSamples, numFeatures = train_x.shape
-print numSamples,numFeatures
-weights = ones((numFeatures, 1))
-print weights
-output=1/(1-exp(-train_x * weights))
-print output
+import logic
+from numpy import *  
+import matplotlib.pyplot as plt  
+import time  
+  
+def loadData():  
+    train_x = []  
+    train_y = []  
+    fileIn = open('./test.txt')  
+    for line in fileIn.readlines():  
+        lineArr = line.strip().split()  
+        train_x.append([1.0, float(lineArr[0]), float(lineArr[1])])  
+        train_y.append(float(lineArr[2]))  
+    return mat(train_x), mat(train_y).transpose()  
+  
+  
+## step 1: load data  
+print "step 1: load data..."  
+train_x, train_y = loadData()  
+test_x = train_x; test_y = train_y  
+  
+## step 2: training...  
+print "step 2: training..."  
+opts = {'alpha': 0.01, 'maxIter': 10000zx, 'optimizeType': 'smoothStocGradDescent'}  
+optimalWeights = trainLogRegres(train_x, train_y, opts)  
+  
+## step 3: testing  
+print "step 3: testing..."  
+accuracy = testLogRegres(optimalWeights, test_x, test_y)  
+  
+## step 4: show the result  
+print "step 4: show the result..."    
+print 'The classify accuracy is: %.3f%%' % (accuracy * 100)  
+showLogRegres(optimalWeights, train_x, train_y)

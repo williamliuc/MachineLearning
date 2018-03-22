f803c6d79ece357108fe1135f81f1c97d9dd18eb
SuperLiu <1476996891@qq.com>
2018-03-22 15:36:10 +0800

    Update plotter.py

--- decisionTree/plotter.py
+++ decisionTree/plotter.py
@@ -1,4 +1,4 @@
-#plotter的第一个版本，可实现带箭头的注解绘制功能
+#根据坐标点画图的第一个版本，可实现带箭头的注解绘制功能
 import matplotlib.pyplot as plt 
 
 #定义文本框和箭头格式
@@ -15,7 +15,7 @@ def createPlot():
 	fig=plt.figure(1,facecolor='white')#figure第一个参数为图像框标题，第二个参数是背景颜色
 	fig.clf()
 	createPlot.ax1=plt.subplot(111,frameon=False)
-	plotNode('decisionNode',(0.5,0.1),(0.1,0.5),decisionNode)
+	plotNode('decisionNode',(0.5,0.1),(0.1,0.5),decisionNode)#第一个坐标为箭头结束位置，第二个坐标是箭头起始地址
 	plotNode('leafNode',(0.8,0.1),(0.3,0.8),leafNode)
 	plt.show()
 

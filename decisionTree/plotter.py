18538797f64bbf6dbe8f8f7758e1d6f5cc4465df
SuperLiu <1476996891@qq.com>
2018-03-22 15:33:25 +0800

    Create plotter.py

---
+++ decisionTree/plotter.py
@@ -0,0 +1,23 @@
+#plotter的第一个版本，可实现带箭头的注解绘制功能
+import matplotlib.pyplot as plt 
+
+#定义文本框和箭头格式
+decisionNode=dict(boxstyle="sawtooth",fc="0.8")
+leafNode=dict(boxstyle="round4",fc="0.8")
+arrow_args=dict(arrowstyle="<-")
+
+#绘制带箭头的注解
+def plotNode(nodeTxt,centerPt,parentPt,nodeType):
+	createPlot.ax1.annotate(nodeTxt,xy=parentPt,xycoords='axes fraction',xytext=centerPt,\
+		textcoords='axes fraction',va="center",ha="center",bbox=nodeType,arrowprops=arrow_args)
+
+def createPlot():
+	fig=plt.figure(1,facecolor='white')#figure第一个参数为图像框标题，第二个参数是背景颜色
+	fig.clf()
+	createPlot.ax1=plt.subplot(111,frameon=False)
+	plotNode('decisionNode',(0.5,0.1),(0.1,0.5),decisionNode)
+	plotNode('leafNode',(0.8,0.1),(0.3,0.8),leafNode)
+	plt.show()
+
+if __name__=="__main__":
+	createPlot()
